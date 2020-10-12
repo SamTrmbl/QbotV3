5eab2968937da25f554d7ba122b66f7e95440dab
Samuel Tremblay <samuel.j.tremblay@gmail.com>
2020-10-12 14:03:55 -0400

    Fonctions base pilotable dans un fichier à part

---
+++ include/Moteur.h
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+#include <Encoder.h> //Installer la librairie Encoder de Paul Stoffregen v1.4.1 dans platformio
+
+
+///////Définition des moteurs de chaque roues
+int vitesse;
+bool direction;
+
+void vitesseRoueLowLevel(int pinA, int pinB, int pinPWM, float vitesse){
+  //TODO ajouter une deadband (voir WPILIB)
+  vitesse = constrain(vitesse,-100,100);// accepte des valeurs de -100 à 100
+  vitesse = map(vitesse,-100,100,-255,255); //transfert ces valeurs sur la bonne plage PWM
+  bool direction = vitesse >= 0; //avancer = True
+  digitalWrite(pinA,!direction); //Pour avancer, la pin A est à 0 et la pin B est à 1
+  digitalWrite(pinB,direction);
+  analogWrite(pinPWM, abs(vitesse));
+}
+
+void vitesseAvG(float vitesse){
+  vitesseRoueLowLevel(34,35,12,vitesse);
+}
+
+void vitesseAvD(float vitesse){
+  vitesseRoueLowLevel(36,37,8,vitesse);//inverse les pins A et B car le moteur de droite doit tourner à l'envers
+}
+
+void vitesseArG(float vitesse){
+  vitesseRoueLowLevel(43,42,9,vitesse);
+}
+
+void vitesseArD(float vitesse){
+  vitesseRoueLowLevel(A5,A4,5,vitesse);//inverse les pins A et B car le moteur de droite tourne à l'envers
+}
+
+////////////Drive modes
+
+void simpleDrive(float vitesse){
+  
+  vitesseAvG(vitesse);
+  vitesseAvD(vitesse);
+  vitesseArG(vitesse);
+  vitesseArD(vitesse);
+}
+
+
+
+/////Encodeurs
+
+Encoder encodeurAvG(18,31);
+
+int getEncodeurAvG(){
+  return encodeurAvG.read();//ne fonctionne pas à valider
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,21 @@
 #include <Arduino.h>
-#include <Manette.h>
+#include <Manette.h>//Le dossier (pas le zip) de la librairie P2X_lib doit être dans le dossier "lib"
+#include <Moteur.h>//La librairie Encoder de Paul Stoffegen v 1.4.1 doit être installée dans Platformio
 
-
-int vitesse;
-bool direction;
-
-void vitesseRoueLowLevel(int pinA, int pinB, int pinPWM, float vitesse){
-  //TODO ajouter une deadband (voir WPILIB)
-  vitesse = constrain(vitesse,-100,100);// accepte des valeurs de -100 à 100
-  vitesse = map(vitesse,-100,100,-255,255); //transfert ces valeurs sur la bonne plage PWM
-  bool direction = vitesse >= 0; //avancer = True
-  digitalWrite(pinA,!direction); //Pour avancer, la pin A est à 0 et la pin B est à 1
-  digitalWrite(pinB,direction);
-  analogWrite(pinPWM, abs(vitesse));
-}
-
-void vitesseAvG(float vitesse){
-  vitesseRoueLowLevel(34,35,12,vitesse);
-}
-
-void vitesseAvD(float vitesse){
-  vitesseRoueLowLevel(36,37,8,vitesse);//inverse les pins A et B car le moteur de droite doit tourner à l'envers
-}
-
-void vitesseArG(float vitesse){
-  vitesseRoueLowLevel(43,42,9,vitesse);
-}
-
-void vitesseArD(float vitesse){
-  vitesseRoueLowLevel(A5,A4,5,vitesse);//inverse les pins A et B car le moteur de droite tourne à l'envers
-}
-
-void simpleDrive(float vitesse){
-  
-  vitesseAvG(vitesse);
-  vitesseAvD(vitesse);
-  vitesseArG(vitesse);
-  vitesseArD(vitesse);
-}
 
 Manette manette;
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
-manette.begin();
+//manette.begin();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //Serial.println(manette.getLX());
-  simpleDrive(manette.getRY());
+  Serial.println(getEncodeurAvG());
+  //simpleDrive(manette.getLY());
+  
 
 }
