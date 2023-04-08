b7cc996f2153dbb07ae48120703a8b41dcbcd196
SamTrmbl <samuel.j.tremblay@gmail.com>
2023-04-08 11:22:43 -0400

    3.2 : Fix automatique pour les deux modèles de shields et changer la nomenclature des boutons

--- include/QBot_Moteur.h
+++ include/QBot_Moteur.h
@@ -37,10 +37,9 @@ void vitesseAvD(int vitesse){
 }
 
 void vitesseArG(int vitesse){
-  /*!!!!!!!!
-  Pour les shields avec un point ROUGE, il faut changer le 9 pour un 6.
-  Enregistre et utilise toujours cette version modifiée de la librairie.
-  !!!!!!!!*/
+  /*Les shields avec un point rouge utilise la pin 6 au lieu de 9.
+  La fonction distanceLowLevel gère automatiquement cette différence
+  en envoyant l'information simultanément aux deux pins*/
   vitesseRoueLowLevel(43,42,9,vitesse);
 }
 
